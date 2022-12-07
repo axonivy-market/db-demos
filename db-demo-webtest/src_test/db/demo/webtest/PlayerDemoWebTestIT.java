5e9549ad6ac1340ed924b9e8a03d3d35614e7ff3
Tobias Gretler <tobias.gretler@axonivy.com>
2022-12-07 16:14:17 +0100

    db-demo-webtest rename SampleWebIT

--- db-demo-webtest/src_test/db/demo/webtest/PlayerDemoWebTestIT.java
+++ db-demo-webtest/src_test/db/demo/webtest/PlayerDemoWebTestIT.java
@@ -10,7 +10,7 @@ import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 
 @IvyWebTest(headless = true) 
-public class SampleWebIT {
+public class PlayerDemoWebTestIT {
 
 	@Test
 	public void fillDialogForm() {
