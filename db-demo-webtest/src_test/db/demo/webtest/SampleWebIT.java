37bc148aacf668423782c385862b97e4367e89a3
Tobias Gretler <tobias.gretler@axonivy.com>
2022-12-07 14:35:32 +0100

    db-demos-webtest

--- db-demo-webtest/src_test/db/demo/webtest/SampleWebIT.java
+++ db-demo-webtest/src_test/db/demo/webtest/SampleWebIT.java
@@ -1,54 +1,28 @@
 package db.demo.webtest;
 
 import static com.codeborne.selenide.Condition.*;
+
 import static com.codeborne.selenide.Selenide.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 
-/**
- * This sample WebTest orchestrates a real browser to 
- * verify that your workflow application and especially it's Html Dialogs are running as expected.
- * 
- * <p>The test can either be run<ul>
- * <li>in the Designer IDE ( <code>right click > run as > JUnit Test </code> )</li>
- * <li>or in a Maven continuous integration build pipeline ( <code>mvn clean verify</code> )</li>
- * </ul></p>
- * 
- * <p>Detailed guidance on writing these kind of tests can be found in our
- * <a href="https://developer.axonivy.com/doc/10.0/concepts/testing/web-testing.html">WebTesting docs</a>
- * </p>
- */
 @IvyWebTest(headless = false)
-public class SampleWebIT{
+public class SampleWebIT {
 
-  @Test
-  public void navigateToInfoPage(){
-    open(EngineUrl.base());
-    
-    $("img").shouldBe(attribute("alt", "Logo"));
-    $(By.tagName("img")).shouldBe(attribute("alt", "Logo"));
-  }
-  
-  @Test
-  @Disabled("illustrative code: needs adaption to your environment")
-  public void fillDialogForm() {
-    // valid links can be copied from the start page of the internal web-browser
-    open(EngineUrl.createProcessUrl("184A3F87F479F367/Update.ivp"));
-    
-    // fill new customer form
-    $(By.id("form:firstname")).sendKeys("Unit");
-    $(By.id("form:lastname")).sendKeys("Test");
-    
-    // verify that the submit button is enabled, before clicking it.
-    $(By.id("form:submit")).shouldBe(enabled).click();
-    
-    // verify that the registration was successful.
-    $(By.id("form:newCustomer")).shouldBe(visible, text("Unit Test"));
-  }
-  
+	@Test
+	public void fillDialogForm() {
+
+		open(EngineUrl.createProcessUrl("db-demo/184BD5B00AD6A83D/start2.ivp"));
+
+		$(By.id("playerInputForm:name")).sendKeys("Tom");
+		$(By.id("playerInputForm:Birthday")).sendKeys("11.11.2010");
+		$(By.id("playerInputForm:FavoriteColor")).sendKeys("Yellow");
+
+		$(By.id("playerInputForm:save")).shouldBe(enabled).click();
+		$(By.id("playerListForm:player:0:delete")).should(enabled).click();
+	}
+
 }
